9622a981a08463b2c525856bde94db3749632c76
Simon Meer <simon.meer@bluewin.ch>
2015-02-22 14:30:36 +0100

    Get SQLite to work with Linq.

--- src/Assets/Scripts/DBTest.cs
+++ src/Assets/Scripts/DBTest.cs
@@ -1,37 +1,57 @@
 ﻿using UnityEngine;
 using System.Collections;
-using Mono.Data.Sqlite;
+using System.Data.Sql;
 using System.Data;
+using System.Data.Linq;
+using System.Linq;
+using System.Data.Linq.Mapping;
 
 public class DBTest : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        string conn = "URI=file:" + Application.dataPath + "/database.s3db"; //Path to database.
-        IDbConnection dbconn;
-        dbconn = (IDbConnection)new SqliteConnection(conn);
-        dbconn.Open(); //Open connection to the database.
-        IDbCommand dbcmd = dbconn.CreateCommand();
+        //string conn = "URI=file:" + Application.dataPath + "/database.s3db"; //Path to database.
+        //var connection = System.Data.SQLite.Linq.SQLiteProviderFactory.Instance.CreateConnection();
+        string _connection_string = "Data Source=" + Application.dataPath + "/database.s3db;DbLinqProvider=sqlite;DbLinqConnectionType=" + typeof(Mono.Data.Sqlite.SqliteConnection).AssemblyQualifiedName
+ + ";";
 
-
-        string sqlQuery = "SELECT value, id " + "FROM TestTable";
-        dbcmd.CommandText = sqlQuery;
-        IDataReader reader = dbcmd.ExecuteReader();
-
-        while (reader.Read())
+        // in database method
+        using (DataContext db = new DataContext(_connection_string))
         {
-            string value = reader.GetString(0);
-            int id = reader.GetInt32(1);
+            var vals = (from values in db.GetTable<TestTable>()
+                         select values);
 
-            Debug.Log("ID = " + id + "  VALUE =" + value );
+            foreach (var val in vals)
+            {
+                Debug.Log(val.Value);
+            }
         }
+        //new SqliteConnection
 
-        reader.Close();
-        reader = null;
-        dbcmd.Dispose();
-        dbcmd = null;
-        dbconn.Close();
-        dbconn = null;
+        //IDbConnection dbconn;
+        //dbconn = (IDbConnection)new SqliteConnection(conn);
+        //dbconn.Open(); //Open connection to the database.
+        //IDbCommand dbcmd = dbconn.CreateCommand();
+
+
+        //string sqlQuery = "SELECT value, id " + "FROM TestTable";
+        //dbcmd.CommandText = sqlQuery;
+        //IDataReader reader = dbcmd.ExecuteReader();
+
+        //while (reader.Read())
+        //{
+        //    string value = reader.GetString(0);
+        //    int id = reader.GetInt32(1);
+
+        //    Debug.Log("ID = " + id + "  VALUE =" + value );
+        //}
+         
+        //reader.Close();
+        //reader = null;
+        //dbcmd.Dispose();
+        //dbcmd = null;
+        //dbconn.Close();
+        //dbconn = null;
  
  
 	}
@@ -40,4 +60,15 @@ public class DBTest : MonoBehaviour {
 	void Update () {
 	
 	}
+
+    [Table(Name = "TestTable")]
+    class TestTable
+    {
+        [Column(IsPrimaryKey = true, IsDbGenerated = true, Name="id")]
+        public int Id;
+        
+        [Column(Name="value")]
+        public string Value;
+
+    }
 }
