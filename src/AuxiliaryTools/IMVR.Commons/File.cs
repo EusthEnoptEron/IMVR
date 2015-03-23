5a7fd8300d3958d8615002fe4160c5c4b2b7a8d1
Simon Meer <simon.meer@bluewin.ch>
2015-03-23 21:01:21 +0100

    Prepare file classes.

---
+++ src/AuxiliaryTools/IMVR.Commons/File.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IMVR.Commons
+{
+    /// <summary>
+    /// Represents an indexed file.
+    /// </summary>
+    public abstract class File
+    {
+        /// <summary>
+        /// Path to the file on the file system.
+        /// </summary>
+        public string Path { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date when the file was indexed.
+        /// </summary>
+        public DateTime Indexed { get; set; }
+
+        /// <summary>
+        /// Gets or sets the date the file was last modified.
+        /// </summary>
+        public DateTime LastModified { get; set; }
+    }
+}
---
+++ src/AuxiliaryTools/IMVR.Commons/Music.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IMVR.Commons
+{
+    /// <summary>
+    /// Represents an indexed audio file.
+    /// </summary>
+    class Music
+    {
+    }
+}
