266861e451878f886e573f7c6365814e7d44a2a9
Simon Meer <simon.meer@bluewin.ch>
2015-04-08 16:43:52 +0200

    Add color console.

---
+++ src/AuxiliaryTools/IMVR.Indexer/Konsole.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMVR.Indexer
+{
+    public static class Konsole
+    {
+        private static object lockObj = new object();
+
+        private static ConsoleColor oldColor;
+        public static void WriteLine(object text, ConsoleColor color = ConsoleColor.Gray)
+        {
+            WriteLine(text.ToString(), color);
+        }
+
+        public static void WriteLine(string text, ConsoleColor color = ConsoleColor.Gray)
+        {
+            lock (lockObj)
+            {
+                var oldColor = Console.ForegroundColor;
+                {
+                    Console.ForegroundColor = color;
+                    Console.WriteLine(text);
+                    Console.ForegroundColor = oldColor;
+                }
+            }
+        }
+        public static void WriteLine(string format, ConsoleColor color = ConsoleColor.Gray, params object[] args) {
+            WriteLine(String.Format(format, args), color);
+        }
+
+    }
+}
